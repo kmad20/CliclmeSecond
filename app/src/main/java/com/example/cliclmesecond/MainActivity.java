0f027c16aa1d894ea9f6b47f7176455e19fb9889
kishan <kishanma2000@iclould.com>
2021-12-19 21:11:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/cliclmesecond/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.cliclmesecond;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+     Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView ot = (TextView)findViewById(R.id.textView);
+                ot.setText("Welcome to android");
+            }
+        });
+
+    }
+}
